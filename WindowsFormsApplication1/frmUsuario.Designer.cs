608d2d68a860c9977c180f66dd5e1b2277c4bd9a
esneyder <edesalla17@gmail.com>
2014-12-19 17:00:30 -0500

    prueba librera

---
+++ WindowsFormsApplication1/frmUsuario.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtclave = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtgenero = new System.Windows.Forms.TextBox();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(9, 172);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(75, 41);
+            this.btnguardar.TabIndex = 0;
+            this.btnguardar.Text = "Save";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtclave);
+            this.groupBox1.Controls.Add(this.txtusuario);
+            this.groupBox1.Controls.Add(this.txtgenero);
+            this.groupBox1.Controls.Add(this.txtapellido);
+            this.groupBox1.Controls.Add(this.txtnombre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnguardar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 228);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mantenimiento usuario";
+            // 
+            // txtclave
+            // 
+            this.txtclave.Location = new System.Drawing.Point(108, 140);
+            this.txtclave.Name = "txtclave";
+            this.txtclave.Size = new System.Drawing.Size(144, 20);
+            this.txtclave.TabIndex = 10;
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(108, 110);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(144, 20);
+            this.txtusuario.TabIndex = 9;
+            // 
+            // txtgenero
+            // 
+            this.txtgenero.Location = new System.Drawing.Point(108, 83);
+            this.txtgenero.Name = "txtgenero";
+            this.txtgenero.Size = new System.Drawing.Size(144, 20);
+            this.txtgenero.TabIndex = 8;
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(108, 54);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(217, 20);
+            this.txtapellido.TabIndex = 7;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(108, 24);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(217, 20);
+            this.txtnombre.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Genero:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apelidos:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 41);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 41);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 278);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmUsuario";
+            this.Text = "Administración usuarios";
+            this.Load += new System.EventHandler(this.frmUsuario_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtclave;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtgenero;
+        private System.Windows.Forms.TextBox txtapellido;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication1/frmUsuario.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using databaseConnection;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmUsuario : Form
+    {
+        DbConnection db = new DbConnection("Parametros.xml");
+        public frmUsuario()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!db.OpenConnection())
+            {
+                db.CloseConnection();
+                MessageBox.Show("Error" + db.Error);
+            }
+            else
+            {
+                string[] parametros = { "@operacion",
+                                      "@nombre", 
+                                      "@apellido", 
+                                      "@genero", 
+                                      "@usuario",
+                                      "@clave" };
+
+                db.ExecuteProcedures("spUsuarioIA",
+                      parametros, "I", txtnombre.Text,
+                                      txtapellido.Text,
+                                      txtgenero.Text,
+                                      txtusuario.Text,
+                                      txtclave.Text);
+                MessageBox.Show("Registro ingresado");
+            }
+
+        }
+
+
+
+
+        private void frmUsuario_Load(object sender, EventArgs e)
+        {
+
+        }
+        public DataTable Persona()
+        {
+
+            String[] parametros = { "@operacion", "@usuarioId" };
+            return db.GetData("spUsuarioSE", parametros, "T", 0);
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!db.OpenConnection())
+            {
+                db.CloseConnection();
+                MessageBox.Show("Error" + db.Error);
+            }
+            else
+            {
+                DataTable dt = new DataTable();
+                string[] parametros = { "@operacion", "@usuarioId" };
+                dt = db.GetData("spUsuarioSE", parametros, "S",7);
+                foreach (DataRow fila in dt.Rows)
+                {
+                    txtnombre.Text = fila["nombre"].ToString();
+                    txtapellido.Text = fila["apellido"].ToString();
+                    txtgenero.Text = fila["genero"].ToString();
+                    txtusuario.Text = fila["usuario"].ToString();
+                    txtclave.Text = fila["clave"].ToString();
+
+
+                }
+                db.CloseConnection();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (!db.OpenConnection())
+            {
+                db.CloseConnection();
+                MessageBox.Show("Error" + db.Error);
+            }
+            else
+            {
+                string[] parametros = { "@operacion",
+                                      "@nombre", 
+                                      "@apellido", 
+                                      "@genero", 
+                                      "@usuario",
+                                      "@clave" };
+
+                db.ExecuteProcedures("spUsuarioIA",
+                      parametros, "A", txtnombre.Text,
+                                      txtapellido.Text,
+                                      txtgenero.Text,
+                                      txtusuario.Text,
+                                      txtclave.Text);
+                MessageBox.Show("Registro Actualizado");
+                db.CloseConnection();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (!db.OpenConnection())
+            {
+                db.CloseConnection();
+                MessageBox.Show("Error" + db.Error);
+            }
+            else
+            {
+                string[] parametros = { "@operacion",
+                                      "@usuarioId", 
+                                       };
+
+                db.ExecuteProcedures("spUsuarioSE",
+                      parametros, "E", 5);
+                MessageBox.Show("Registro eliminado");
+                db.CloseConnection();
+            }
+        }
+    }
+}
